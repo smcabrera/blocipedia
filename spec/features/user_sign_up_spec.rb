0b28b855bf7654698ccb7dab582eceff31048fde
Stephen Mariano Cabrera <stephen.m.cabrera@gmail.com>
2015-04-06 15:17:42 -0500

    User sign up, sign in and sign out

--- spec/features/user_sign_up_spec.rb
+++ spec/features/user_sign_up_spec.rb
@@ -1,20 +1,74 @@
 require 'rails_helper'
 require 'spec_helper'
+require 'pry'
 
-describe 'New user sign up' do
-  it 'user can sign up by supplying a user name and password' do
+feature 'New user signs up' do
+  scenario 'with invalid username and password' do
+    visit root_path
+    click_link "Sign up"
+    fill_in 'user_email', with: 'noatsign'
+    fill_in 'user_password', with: 'password'
+    fill_in 'user_password_confirmation', with: 'password'
+
+    expect(User.all.count).to eq(0)
+  end
+
+  scenario 'with valid username and password' do
     visit root_path
     click_link "Sign up"
 
-    expect(page).to have_content("Foo!")
-
-    fill_in 'user_email', with: 'test@example.com'
-    fill_in 'user_password', with: 's3kr3t'
-    fill_in 'user_password_confirmation', with: 's3kr3t'
+    fill_in 'user_email', with: 'userA@example.com'
+    fill_in 'user_password', with: 'password'
+    fill_in 'user_password_confirmation', with: 'password'
 
     click_button 'Sign up'
 
-    expect(page).to have_content('Hello')
+    expect(User.all.count).to eq(1)
+  end
+end
+
+feature 'User sign in' do
+  scenario 'user supplies valid credentials' do
+    User.create(
+    :email => 'username@example.com',
+    :password => 'password'
+    )
+
+    visit root_path
+    click_link "Sign in"
+    fill_in 'user_email', with: 'username@example.com'
+    fill_in 'user_password', with: 'password'
+    click_button 'Log in'
+
+    expect(page).to have_content('Sign out')
+  end
+
+  scenario 'user supplies invalid credentials' do
+    visit root_path
+    click_link "Sign in"
+    fill_in 'user_email', with: 'invalid@bogus.com'
+    fill_in 'user_password', with: 'WrongPassword'
+    click_button 'Log in'
+
+    expect(page).to have_content("Log in")
+  end
+end
+
+feature 'User sign out' do
+  scenario 'user signs in and then signs out' do
+    User.create(
+      :email => 'userC@example.com',
+      :password => 'password'
+    )
+
+    visit root_path
+    click_link "Sign in"
+    fill_in 'user_email', with: 'userC@example.com'
+    fill_in 'user_password', with: 'password'
+    click_button 'Log in'
+    click_link "Sign out"
+
+    expect(page).to have_content('Sign in')
   end
 end
 
