d8deda8e5eb026ced9cf0bf86e1c82d4fa8f30f7
Stephen Mariano Cabrera <stephen.m.cabrera@gmail.com>
2015-04-15 19:18:01 -0500

    Format markdown in wiki bodies and titles

--- app/models/wiki.rb
+++ app/models/wiki.rb
@@ -2,10 +2,4 @@ class Wiki < ActiveRecord::Base
   belongs_to :user
   validates :user, presence: true
 
-  def markdown_to_html(text)
-    renderer = Redcarpet::Render::HTML.new
-    extensions = { fenced_code_blocks: true }
-    redcarpet = Redcarpet::Markdown.new(renderer, extensions)
-    redcarpet.render(text).html_safe
-  end
 end
