dd727e027e3d0119c2a4341846d4d5b8c4a6fa8b
Stephen Mariano Cabrera <stephen.m.cabrera@gmail.com>
2015-04-27 20:17:04 -0500

    Add methods to work with collaborators

--- app/models/wiki.rb
+++ app/models/wiki.rb
@@ -1,7 +1,22 @@
 class Wiki < ActiveRecord::Base
   belongs_to :user
   has_many :collaborations
-  has_many :users, through: :collaborations
-
   validates :user, presence: true
+
+  after_save :set_owner_as_collaborator
+
+  def add_collaborator(user)
+    Collaboration.create(:user_id => user.id, :wiki_id => self.id)
+  end
+
+  def collaborators
+    self.collaborations.map { |c| c.user }
+  end
+
+  private
+
+  def set_owner_as_collaborator
+    user = self.user
+    Collaboration.create(:user_id => user.id, :wiki_id => self.id)
+  end
 end
